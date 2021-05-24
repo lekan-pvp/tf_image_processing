570c73f06af58a8f90c88b1e3cb7b6c7add53a21
Vladislav Popov <lekan.lekan@gmail.com>
2021-05-24 21:14:37 +0300

    PIL

--- image_proc.py
+++ image_proc.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from PIL import Image, ImageFilter
+
 
 # Decode image data from a file in Tensorflow
 def decode_image(filename, image_type, resize_shape, channels=0):
@@ -33,5 +35,14 @@ def get_image_data(image_paths, iamge_type=None, resize_type=None, channels=0):
             image_data_list.append(image_data)
     return image_data_list
 
+# Load and resize an image using PIL, and return its pixel data
+def pil_resize_image(image_path, resize_shape, image_mode='RGBA', image_filter=None):
+    im = Image.open(image_path)
+    converted_im = im.convert(image_mode)
+    resized_im = converted_im.resize(resize_shape, Image.LANCZOS)
+    if image_filter != None:
+        resized_im = resized_im.filter(image_filter)
+    im_data = resized_im.getdata()
+    return np.asarray(im_data)
 
 
